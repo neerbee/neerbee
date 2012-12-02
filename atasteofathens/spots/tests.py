3089b999c0d34d23bd4b00b8fff4cf85ef666c29
greenonion <sublime.gr@gmail.com>
2012-12-02 20:29:59 +0200

    tests

--- atasteofathens/spots/tests.py
+++ atasteofathens/spots/tests.py
@@ -3,14 +3,6 @@ from atasteofathens.spots.models import Spot
 
 class SpotModelTest(MongoTestCase):
     def test_creating_a_new_spot(self):
-        pass
-"""
-from mongoengine.django.tests import MongoTestCase
-from atasteofathens.spots.models import Spot
-from nose.tools import assert_equals
-
-class SpotModelTest(object):
-    def test_creating_a_new_spot_and_saving_it_to_the_database(self):
         # start by creating a new Spot object with its "name" set
         spot = Spot()
         spot.name = "The Diner"
@@ -28,4 +20,3 @@ class SpotModelTest(object):
         # and check that it has saved its two attributes: name and address
         self.assertEquals(only_spot_in_database.name, "The Diner")
         self.assertEquals(only_spot_in_database.address, spot.address)
-        """
