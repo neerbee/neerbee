ad83e8c353e47167590eef5f4579378246c62504
greenonion <sublime.gr@gmail.com>
2012-12-09 11:11:05 +0200

    fixed comments

--- atasteofathens/spots/views.py
+++ atasteofathens/spots/views.py
@@ -1,5 +1,5 @@
-#from django.shortcuts import render, render_to_response, get_object_or_404
-from coffin.shortcuts import render, render_to_response, get_object_or_404
+from django.shortcuts import get_object_or_404
+from coffin.shortcuts import render
 from django.contrib.auth.decorators import login_required
 from django.contrib.auth.models import User
 from atasteofathens.spots.models import Spot
@@ -15,26 +15,25 @@ def users(request):
     user_list = User.objects.all()
     return render(request, 'spots/user_list.html', {'user_list': user_list})
 
-"""
 @login_required
 def user_profile(request, user_name):
     u = get_object_or_404(User, username=user_name)
     return render(request, 'spots/user_profile.html', {'user': u})
 
-
+"""
 @login_required
 def user_rate(request, user_name):
     u = get_object_or_404(User, username=user_name)
-
+"""
     #all_items = Restaurant.objects.all()
 
-    user_ratings = [(i.restaurant, i.rating_value) for i in u.u_ratings.all()]
+   # user_ratings = [(i.restaurant, i.rating_value) for i in u.u_ratings.all()]
     #rated_items = [x[0] for x in user_ratings]
     #no_rating = [(i, None) for i in all_items if i not in rated_items]
 
     #user_ratings = user_ratings + no_rating
     #user_ratings.sort(key=lambda item: item[0].name)
-
+"""
     return render(request, 'spots/user_rate.html', 
                              {'user': u,
                               'user_ratings': user_ratings})
