e97d37a519c13094ef34a48d2ddc2b8de50effcc
greenonion <sublime.gr@gmail.com>
2012-12-20 21:50:11 +0200

    custom user model

--- neerbee/users/views.py
+++ neerbee/users/views.py
@@ -1,7 +1,8 @@
 from django.shortcuts import render
 from mongoengine.django.shortcuts import get_document_or_404
 from django.contrib.auth.decorators import login_required
-from mongoengine.django.auth import User
+#from mongoengine.django.auth import User
+#from neerbee.users.models import Bee
 
 @login_required
 def user_home(request):
