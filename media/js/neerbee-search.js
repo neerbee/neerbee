c3cdd34e5eb143c403d94f336cdbc387ec85c071
Antony Dmnx <lib3rat0r@gmail.com>
2012-12-28 15:11:05 +0200

    search working

--- media/js/neerbee-search.js
+++ media/js/neerbee-search.js
@@ -1,104 +1,105 @@
 $(function() {
 
-	var NeerbeeSearch = {};
-	window.NeerbeeSearch = NeerbeeSearch;
+	var Neerbee = {};
+	window.Neerbee = Neerbee;
 
-	var template = function(name) {
-		return Mustache.compile($('#'+name+'-template').html());
-	};
+	var template = function(name) { return Mustache.compile($('#'+name+'-template').html()); };
 
-	NeerbeeSearch.Result = Backbone.Model.extend({
-		name: function() { return this.get('name'); },
-		location: function() { return this.get('location'); }
-	});
-
-	NeerbeeSearch.Results = Backbone.Collection.extend({
-		model: NeerbeeSearch.Result,
-		url: "/api/v1/spot/search/?format=json"
-	});
-
-	NeerbeeSearch.Index = Backbone.View.extend({
-		template: template('index'),
-		initialize: function() {
-			this.results = new NeerbeeSearch.Results();
-			this.results.fetch({ "q": $("#input-search-query").val() });
-		},
-		render: function() {
-			this.$el.html(this.template(this));
-			var resultsView = new NeerbeeSearch.Index.Results({ collection: this.results });
-			var form = new NeerbeeSearch.Index.Form({ collection: this.results });
-			this.$('.results').append(form.render().el);
-			this.$('.results').append(resultsView.render().el);
-			return this;
-		},
-		count: function() {
-			return this.results.length;
-		}
-	});
-
-	NeerbeeSearch.Index.Form = Backbone.View.extend({
-		tagName: 'form',
-		className: 'form',
-		template: template('index-form'),
-		events: {
-			'keypress #input-search-query': 'displaySearch'
-		},
-		render: function() {
-			this.$el.html(this.template(this));
-			return this;
-		},
-		displaySearch: function(event) {
-			this.collection.fetch({ "q": $("#input-search-query").val() });
-		}
-	});
-
-	NeerbeeSearch.Index.Result = Backbone.View.extend({
-		tagName: 'li',
-		template: template('index-result'),
-		events: {
-			// TODO: click on result
-		},
-		render: function() {
-			this.$el.html(this.template(this));
-			return this;
-		},
-		name: function() { return this.model.get('name'); },
-		//location: function() { return this.model.get('location'); }
-		// TODO: click function
-	});
-
-	NeerbeeSearch.Index.Results = Backbone.View.extend({
-		tagName: 'ul',
-		render: function() {
-			this.collection.each(
-				function(result) {
-					var view = new NeerbeeSearch.Index.Result({ model: result });
-					this.$el.append(view.render().el);
-				},
-				this
-			);
-			return this;
-		}
-	});
-
-	NeerbeeSearch.Router = Backbone.Router.extend({
+	Neerbee.Router = Backbone.Router.extend({
 		initialize: function(options) {
 			this.el = options.el;
 		},
 		routes: {
-			"": "index"
+			"": "search"
 		},
-		index: function() {
-			var view = new NeerbeeSearch.Index();
+		search: function() {
+			var view = new Neerbee.Search();
 			this.el.empty();
 			this.el.append(view.render().el);
 		}
 	});
 
-	NeerbeeSearch.boot = function(container) {
+	Neerbee.boot = function(container) {
 		container = $(container);
-		var router = new NeerbeeSearch.Router({ el: container });
+		var router = new Neerbee.Router({ el: container });
 		Backbone.history.start();
 	}
+
+	Neerbee.Result = Backbone.Model.extend({
+		name: function() { return this.get('name'); }
+	});
+
+	Neerbee.Results = Backbone.Collection.extend({
+		model: Neerbee.Result,
+		url: "/api/v1/spot/search/"
+	});
+
+	Neerbee.Search = Backbone.View.extend({ //
+		initialize: function() {
+			this.results = new Neerbee.Results();
+			this.results.on('all', this.showResults, this);
+		},
+		render: function() {
+
+			var resultsView = new Neerbee.Search.Results({ collection: this.results });
+
+			$("#input-search-query").keyup(function() {
+				resultsView.results().fetch({data:{"q":$("#input-search-query").val()}});
+			});
+
+			this.$el.append(resultsView.render().el);
+			return this;
+
+		},
+		showResults: function() {
+
+			var resultsView = new Neerbee.Search.Results({ collection: this.results });
+
+			this.$el.empty();
+			this.$el.append(resultsView.render().el);
+
+		}
+	});
+
+	Neerbee.Search.Results = Backbone.View.extend({
+		tagName: 'ul',
+		className: 'nav nav-tabs nav-stacked results-list',
+		render: function() {
+			for (var i = 0; i < this.collection.length; i++) {
+				var thisModel = this.collection.models[i];
+				if (thisModel.attributes.models[0]) {
+					this.renderItem(thisModel);
+				}
+			};
+			return this;
+		},
+		renderItem: function (model) {
+			var item = new Neerbee.Search.Result({
+				"model": model
+			});
+			item.render().$el.appendTo(this.$el);
+		},
+		results: function() {
+			return this.collection;
+		}
+	});
+
+	Neerbee.Search.Result = Backbone.View.extend({
+		tagName: "li",
+		className: "result",
+		template: template('result'),
+		render: function () {
+			this.$el.html(this.template(this));
+			return this;
+		},
+		name: function() { 
+			var thisName = this.model.attributes.models[0].name;
+			thisName = thisName.replace($("#input-search-query").val(), '<span style="background-color: PaleTurquoise;">' + $("#input-search-query").val() + '</span>');
+			return thisName; 
+		},
+		address: function() { return this.model.attributes.models[0].address; },
+		neighbourhood: function() { return this.model.attributes.models[0].neighbourhood; },
+		slug: function() { return this.model.attributes.models[0].slug; }
+	});
 
 });
