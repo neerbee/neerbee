50ec0533d9b9784bf58664915c8a32f44b20898a
greenonion <sublime.gr@gmail.com>
2013-02-16 15:01:56 +0200

    heroku settings

--- neerbee/neerbee/settings/staging.py
+++ neerbee/neerbee/settings/staging.py
@@ -67,7 +67,7 @@ MEDIA_URL = '/media/'
 # Don't put anything in this directory yourself; store your static files
 # in apps' "static/" subdirectories and in STATICFILES_DIRS.
 # Example: "/home/media/media.lawrence.com/static/"
-STATIC_ROOT = PROJECT_ROOT.child('static')
+STATIC_ROOT = ('./static')
 #STATIC_ROOT = ''
 
 # URL prefix for static files.
