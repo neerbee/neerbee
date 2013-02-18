0ac5570080b9ffc29c03537d6226e044b3e95001
greenonion <sublime.gr@gmail.com>
2013-02-18 10:05:40 +0200

    heroku staticfiles

--- neerbee/neerbee/urls.py
+++ neerbee/neerbee/urls.py
@@ -1,5 +1,6 @@
 from django.conf.urls import patterns, include, url
 from django.views.generic.simple import direct_to_template
+from django.conf import settings
 
 from tastypie.api import Api
 
@@ -44,5 +45,5 @@ urlpatterns = patterns('',
 )
 
 urlpatterns += patterns('',  
-    (r'^static/(?P<path>.*)$', 'django.views.static.serve', {'document_root': settings.staging.STATIC_ROOT}),  
+    (r'^static/(?P<path>.*)$', 'django.views.static.serve', {'document_root': settings.STATIC_ROOT}),  
 )  
